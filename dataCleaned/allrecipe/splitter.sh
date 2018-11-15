c91ffaec143c2bcabd7de3fc360096c797cc65bc
TTimTT <tim.tuuva@epfl.ch>
2018-11-15 15:41:55 +0100

    Extracted urls from html index pages for each region

--- dataCleaned/allrecipe/splitter.sh
+++ dataCleaned/allrecipe/splitter.sh
@@ -5,7 +5,9 @@ STARTING=$PWD
 for directory in $(find $STARTING -type d); 
 do
 	cd "$directory"
-	url=$(cat *.html* | grep "canonical*"  | sed "s/.*href=\"//" | sed "s/\" \/>/?page=/")
-	$STARTING/./crawling.sh $url 1000
+	#url=$(cat *.html* | grep "canonical*"  | sed "s/.*href=\"//" | sed "s/\" \/>/?page=/")
+	url=$(cat index.html\?page\=* | grep -A 3 "<article class=\"fixed-recipe-card\">" | grep "href" | sed "s/<a href=\"//" | sed "s/\" data.*//")
+	echo "$url" > "urls.txt"
+	#$STARTING/./crawling.sh $url 1000
 	cd $STARTING
 done
